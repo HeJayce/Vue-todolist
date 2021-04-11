941d09f607eedf967f0e7d57cc60092d36d4ca42
HeJayce <hejayce@gmail.com>
2021-04-11 21:28:40 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>This is App</div>
+</template>
+
+<script>
+import img from './assets/images/bg.jpeg'
+import './assets/images/head.jpeg'
+import  './assets/styles/test.css'
+
+export default {
+  name: "App"
+}
+</script>
+
+<style scoped>
+
+</style>
