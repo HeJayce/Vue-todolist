f1951b4a298ab64576c38da0ca18d38980c1eae9
HeJayce <hejayce@gmail.com>
2021-04-11 22:19:53 +0800

    init

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,21 @@
 <template>
-  <div>This is App</div>
+  <div><h1>This is App</h1></div>
 </template>
 
 <script>
-import img from './assets/images/bg.jpeg'
-import './assets/images/head.jpeg'
-import  './assets/styles/test.css'
+// import img from './assets/images/bg.jpeg'
+// import './assets/images/head.jpeg'
+// import  './assets/styles/test.css'
+
+import './assets/styles/global.styl'
 
 export default {
   name: "App"
 }
 </script>
 
-<style scoped>
-
+<style lang="stylus" scoped>
+h1
+  color: red
+  transform translate(100px,100px)
 </style>
--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,10 @@
 // 使用node的path模块
 const path = require('path')
-
 // 引入vue-loader插件
 const VueLoaderPlugin = require('vue-loader/lib/plugin')
+
+const htmlWebpackPlugin = require('html-webpack-plugin')
+
 
 module.exports = {
     mode: "development",
@@ -29,13 +31,24 @@ module.exports = {
             },{
             test: /\.css/,
                 use: ['style-loader','css-loader']
+            },{
+                test: /\.styl(us)?$/,
+                use: [
+                    'vue-style-loader',
+                    'css-loader',
+                    'postcss-loader',
+                    'stylus-loader'
+                ]
             }
 
         ]
     },
     plugins: [
         // 请确保引入这个插件！
-        new VueLoaderPlugin()
+        new VueLoaderPlugin(),
+        new htmlWebpackPlugin({
+            template: './index.html'
+        }),
     ],
     resolve: {
         alias: {
