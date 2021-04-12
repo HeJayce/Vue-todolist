fcbe717a85547e11ac681b0fb8be34c60b5f1d0a
HeJayce <hejayce@gmail.com>
2021-04-12 21:01:58 +0800

    add and del

--- src/components/MainTodo/coms/todoItem.vue
+++ src/components/MainTodo/coms/todoItem.vue
@@ -1,13 +1,80 @@
 <template>
-
+  <div :class="['todo-item',todo.completed ? 'completed' : '']">
+    <input type="checkbox" v-model="todo.completed"/>
+    <label>{{todo.content}}</label>
+    <button @click="delItem"></button>
+  </div>
 </template>
 
 <script>
 export default {
-  name: "todoItem"
+  name: "todoItem",
+  props: {
+    todo: Object
+  },
+  methods: {
+    delItem(){
+      this.$emit('del',this.todo.id)
+
+    }
+  }
 }
 </script>
 
-<style scoped>
+<style lang="stylus" scoped>
+@import "~styles/theme.styl"
+@import "~styles/mixins.styl"
+
+.todo-item
+
+  display flex
+  justify-content space-between
+  padding 10px
+  font-size 24px
+  border-top 1px solid rgba(0,0,0,0.1)
+
+  &:hover
+    button:after
+      content 'x'
+      font-size 24px
+      color $lightred
+  &.completed
+    label
+      color red
+      text-decoration line-through
+
+
+  input
+    width: 50px
+    height: 30px
+    cleanDefaultStyle()
+    //appearance none
+    //border none
+    //outline none
+    text-align center
+    &:after
+      content url('~images/arrow-right-circle.svg')
+    &:checked:after
+      content url('~images/success.svg')
+
+  label
+    flex 1
+    line-height 30px
+    transition color 0.4s
+
+  button
+    width: 40px
+    background-color transparent
+    //appearance none
+    //border none
+    //outline none
+    cleanDefaultStyle()
+    cursor: pointer
+
+
+
+
+
+
 
 </style>
--- src/components/MainTodo/mainTodo.vue
+++ src/components/MainTodo/mainTodo.vue
@@ -1,12 +1,49 @@
 <template>
   <div class="main-todo">
-    <input type="text" class="add-todo" placeholder="what to do" autofocus>
+    <input type="text"
+           class="add-todo"
+           placeholder="what to do"
+           autofocus
+    @keyup.enter="addTodo"
+    v-model="content">
+    <todo-item v-for="(item,index) in todoData" :key="index" :todo="item" @del="handDelItem">
+
+
+    </todo-item>
   </div>
 </template>
 
 <script>
+import todoItem from "./coms/todoItem.vue";
+let id = 0
 export default {
-name: "mainTodo"
+  name: "mainTodo",
+  components: {
+    todoItem
+  },
+  data(){
+    return {
+      content:'',
+      todoData: []
+    }
+  },
+  methods: {
+    addTodo(){
+      if (this.content === '') return
+      this.todoData.unshift(
+          {
+            id: id++,
+            content: this.content,
+            completed: false
+          }
+      )
+      this.content = ''
+      console.log(this.todoData)
+    },
+    handDelItem(id){
+      this.todoData.splice(this.todoData.findIndex(item => item.id === id ), 1)
+    }
+  }
 }
 </script>
 
@@ -17,6 +54,7 @@ name: "mainTodo"
   background-color #ffffff
   box-shadow 0 0 5px #666
   .add-todo
+    padding: 16px 16px 16px 36px
     width: 100%
     font-size 24px
     font-weight inherit
@@ -24,6 +62,8 @@ name: "mainTodo"
     color inherit
     border none
     outline none
+    box-sizing: border-box
+
 
 
 </style>
